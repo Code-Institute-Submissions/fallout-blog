f377d3f8216b5a85547fafb42ec1c08b488ea72c
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-12-10 05:15:44 +0000

    Install pillow, write post model, migrate changes. Create superuser. Add content to readme

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    """
+    A model for creating a table where posts will be written
+    and what information the posts will require
+    """
+
+    STATUS = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+        )
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique_for_date='created')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    posted_on = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS, default='draft')
