34adbc973219a7d1c4f450ef1738d5e50370ee52
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-12-12 01:41:02 +0000

    Add forms.py create comment from, add comment ability to post_detail.html

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from .models import Post
+from .forms import CommentForm
 
 
 def HomeView(request):
@@ -16,4 +17,16 @@ def Post_View(request, post):
     A view for looking at the details of a blog post
     """
     post = get_object_or_404(Post, slug=post, status='published')
-    return render(request, 'post_detail.html', {'post': post})
+    comments = post.comments.filter(status=True)
+    user_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            user_comment = comment_form.save(commit=False)
+            user_comment.post = post
+            user_comment.save()
+            return HttpResponseRedirect('/' + post.slug)
+    else:
+        comment_form = CommentForm()
+    return render(request, 'post_detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form, })
